3067e81507647607409a3e5800d4620a880c431c
Elis Jones <ej14433@my.bristol.ac.uk>
2019-04-30 16:45:37 +0100

    first pass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/layout";
+import Home from "../components/home";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = ({
+    data: {
+        allMarkdownRemark: { edges },
+    },
+}) => {
+    const Posts = edges
+        .filter(({ node }) => !!node.frontmatter.title)
+        .map(({ node }) => (
+            <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+        ));
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    return (
+        <Layout>
+            <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+            <Home>{Posts}</Home>
+        </Layout>
+    );
+};
 
-export default IndexPage
+export default IndexPage;
+
+export const pageQuery = graphql`
+    query {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+            edges {
+                node {
+                    html
+                    frontmatter {
+                        title
+                        path
+                    }
+                }
+            }
+        }
+    }
+`;
